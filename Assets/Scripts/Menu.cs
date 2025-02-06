4fef4c9821e834993d2508e41ccbae0cf84bb403
Teo Kai Xiang <114890789+moshiew@users.noreply.github.com>
2025-02-06 14:30:52 +0800

    Adjusted Menu Font, Fixed AR Image Tracking for first enemy

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PrefabCreator.cs
+++ Assets/Scripts/PrefabCreator.cs
@@ -10,20 +10,51 @@ public class PrefabCreator : MonoBehaviour
 
     private GameObject enemy;
     private ARTrackedImageManager arTrackedImageManager;
+    private Transform cameraTransform;
 
     private void OnEnable()
     {
         arTrackedImageManager = gameObject.GetComponent<ARTrackedImageManager>();
         arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+
+        cameraTransform = Camera.main.transform;
     }
 
     private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
     {
         foreach (ARTrackedImage image in obj.added)
         {
-            enemy = Instantiate(enemyPrefab, image.transform);
+            enemy = Instantiate(enemyPrefab, image.transform.position, image.transform.rotation);
             enemy.transform.position += prefabOffset;
+            enemy.transform.SetParent(image.transform);
+        }
+
+        foreach (ARTrackedImage image in obj.removed)
+        {
+            if(enemy!= null)
+            {
+                Destroy(enemy);
+            }
         }
     }
 
+    private void Update()
+    {
+        if(enemy!= null)
+        {
+            LookAtCamera();
+        }
+    }
+
+    private void LookAtCamera()
+    {
+        Vector3 directionToCamera = cameraTransform.position - enemy.transform.position;
+
+        directionToCamera.y = 0;
+
+        if(directionToCamera != Vector3.zero)
+        {
+            enemy.transform.rotation = Quaternion.LookRotation(directionToCamera);
+        }
+    }
 }
