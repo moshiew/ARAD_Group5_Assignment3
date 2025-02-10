105d6c13a6ae171dd271f8a48e927862c271d5f5
Teo Kai Xiang <114890789+moshiew@users.noreply.github.com>
2025-02-10 10:43:19 +0800

    Added Enemy Prefab List

--- Assets/Scripts/PrefabCreator.cs
+++ Assets/Scripts/PrefabCreator.cs
@@ -5,12 +5,13 @@ using UnityEngine.XR.ARFoundation;
 
 public class PrefabCreator : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private List<GameObject> enemyPrefabs;
     [SerializeField] private Vector3 prefabOffset;
 
-    private GameObject enemy;
     private ARTrackedImageManager arTrackedImageManager;
     private Transform cameraTransform;
+
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
 
     private void OnEnable()
     {
@@ -24,29 +25,43 @@ public class PrefabCreator : MonoBehaviour
     {
         foreach (ARTrackedImage image in obj.added)
         {
-            enemy = Instantiate(enemyPrefab, image.transform.position, image.transform.rotation);
-            enemy.transform.position += prefabOffset;
-            enemy.transform.SetParent(image.transform);
+            int index = GetImageIndex(image.referenceImage.name);
+
+            Debug.Log("Image Tracked: " + image.referenceImage.name + " - Prefab Index: " + index);
+
+            if (index >= 0 && index < enemyPrefabs.Count)
+            { 
+                GameObject enemy = Instantiate(enemyPrefabs[index], image.transform.position, image.transform.rotation);
+                enemy.transform.position += prefabOffset;
+                enemy.transform.SetParent(image.transform);
+
+                spawnedEnemies.Add(enemy);
+            }
         }
 
         foreach (ARTrackedImage image in obj.removed)
         {
-            if(enemy!= null)
+            Transform enemyTransform = image.transform.GetChild(0);
+            if(enemyTransform != null)
             {
-                Destroy(enemy);
+                spawnedEnemies.Remove(enemyTransform.gameObject);
+                Destroy(enemyTransform.gameObject);
             }
         }
     }
 
     private void Update()
     {
-        if(enemy!= null)
+        foreach(GameObject enemy in spawnedEnemies)
         {
-            LookAtCamera();
+            if(enemy != null)
+            {
+                LookAtCamera(enemy);
+            }
         }
     }
 
-    private void LookAtCamera()
+    private void LookAtCamera(GameObject enemy)
     {
         Vector3 directionToCamera = cameraTransform.position - enemy.transform.position;
 
@@ -56,5 +71,17 @@ public class PrefabCreator : MonoBehaviour
         {
             enemy.transform.rotation = Quaternion.LookRotation(directionToCamera);
         }
+    }
+
+    private int GetImageIndex(string imageName)
+    {
+        for(int i = 0; i < arTrackedImageManager.referenceLibrary.count; i++)
+        {
+            if (arTrackedImageManager.referenceLibrary[i].name == imageName)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
