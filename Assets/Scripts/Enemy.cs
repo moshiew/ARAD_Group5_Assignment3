91582ca466a1c7385915ea70b31cb5d3f229404d
r3bbitiscute <chongkhaixuen0314@gmail.com>
2025-02-11 11:18:22 +0800

    Fixed Animation Issue

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     private Transform cameraTransform;
+    private Animator animator;
     [SerializeField] private float speed;
+    [HideInInspector] public int health;
+
+    public Image[] healthIcons;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
+        animator = GetComponent<Animator>();
+        health = healthIcons.Length;
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) Damaged();
         Vector3 targetPosition = cameraTransform.position;
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    public void Damaged()
+    {
+        health--;
+        animator.SetTrigger("Damaged");
+
+        for (int i = 0; i < healthIcons.Length; i++)
+        {
+            healthIcons[i].gameObject.SetActive(i < health); // Do a check of the health and the length of the array
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
