faf2a85c9686de8baf0b71a7dda36ba8420b5276
jungwoochoi2005 <jungwoochoi2005@gmail.com>
2025-02-12 16:14:04 +0800

    Testing touch to hit the enemy beta 1.

--- Assets/Scripts/Tapper.cs
+++ Assets/Scripts/Tapper.cs
@@ -1,17 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Tapper : MonoBehaviour
 {
     public Touch tap;
     public Enemy enemy;
-
+    public GameObject sensor;
+    RaycastHit hit;
+    void Start()
+    {
+        sensor = gameObject.GetComponent<GameObject>();
+        sensor = GameObject.Find("TouchSensor");
+    }
     void Update()
     {
+        CheckForEnemies();
         if (Input.touchCount > 0)
         {
-            enemy.Damaged();
+            Debug.Log(Input.touchCount);
+            Touch tap = Input.GetTouch(0);
+            Debug.Log(tap.position);
+            sensor.transform.position = Camera.main.ScreenToViewportPoint(tap.position);
+            if (enemy != null)
+            {
+                Debug.Log(Physics.Raycast(sensor.transform.position, enemy.transform.position));
+                if (Physics.Raycast(sensor.transform.position, enemy.transform.position))
+                {
+                    enemy.Damaged();
+                }
+            }
+        }
+    }
+    void CheckForEnemies()
+    {
+        if (FindAnyObjectByType<Enemy>() != null && enemy == null)
+        {
+            Debug.Log("Enemy found.");
+            enemy = FindAnyObjectByType<Enemy>();
+
         }
     }
 }
